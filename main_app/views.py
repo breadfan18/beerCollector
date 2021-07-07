74ae08f334c7da8ea614ddc4040889a2e68252fa
Swaroop <breadfan18@gmail.com>
2021-07-07 11:27:58 -0400

    added the class based views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Beer
+from main_app import models
 
 # Create your views here.
 def home(request):
@@ -15,3 +17,15 @@ def beers_index(request):
 def beers_detail(request, beer_id):
     beer = Beer.objects.get(id=beer_id)
     return render(request, 'beers/detail.html', {'beer': beer})
+
+class BeerCreate(CreateView):
+    model = Beer
+    fields = '__all__'
+
+class BeerUpdate(UpdateView):
+    model = Beer
+    fields = ['type', 'alcPercent', 'abuLevel', 'origin']
+
+class BeerDelete(DeleteView):
+    model = Beer
+    success_url = '/beers/'
