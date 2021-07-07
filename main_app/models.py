c463f5b651a3605c27e08816ca92a918a629c868
Swaroop <breadfan18@gmail.com>
2021-07-07 11:41:01 -0400

    beer create is complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Beer(models.Model):
@@ -10,3 +11,6 @@ class Beer(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'beer_id': self.id})
