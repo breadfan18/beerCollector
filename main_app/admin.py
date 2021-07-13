ec979547cf73d35dfc3209c685fef4c8859f3364
Swaroop <breadfan18@gmail.com>
2021-07-13 17:04:53 -0400

    added markup to show awards for beer detail

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Beer, Drinking
+from .models import Beer, Drinking, Award
 
 admin.site.register(Beer)
 admin.site.register(Drinking)
+admin.site.register(Award)
